7035e4c1f4093f04246d75647f130f4b955010e7
motchie8 <m.naoki1224@gmail.com>
2018-12-23 18:18:31 +0900

    modified build settings for neovim

--- bin/install-yum.sh
+++ bin/install-yum.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-# install libs for pyenv 
-sudo yum install git xsel zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel \
+# install libraries for pyenv and neovim 
+sudp yum update -y && sudo yum install git xsel zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel \
 openssl-devel xz xz-devel libffi-devel make libtool autoconf automake cmake gcc gcc-c++ make pkgconfig unzip gettext patch -y
+
 # install pyenv
 curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
 echo '
@@ -19,16 +20,17 @@ pyenv install 3.5.6
 pyenv virtualenv 2.7.15 neovim2
 pyenv virtualenv 3.5.6 neovim3
 
-# setup python libs for neovim
+# setup python libs for each env
 pyenv rehash
 source ~/.bash_profile
 pyenv activate neovim2
+pip install --upgrade pip
 pip install neovim
 pyenv deactivate
 pyenv activate neovim3
-pip install -U pip
+pip install --upgrade pip
 pip install -r ~/.dotfiles/python/requirements.txt
-# create symbolic links for nvim pyenv
+# create symbolic links
 ln -s `pyenv which flake8` ~/.pyenv/bin/flake8
 #ln -s `pyenv which flake8-import-order` ~/.pyenv/bin/flake8-import-order
 ln -s `pyenv which autopep8` ~/.pyenv/bin/autopep8
@@ -39,10 +41,12 @@ ln -s `pyenv which isort` ~/.pyenv/bin/isort
 cd ~/.dotfiles
 git clone https://github.com/neovim/neovim
 cd neovim
-make
+make clean
+make CMAKE_BUILD_TYPE=Release
 sudo make install
 sudo ln -s -T /usr/local/bin/nvim /usr/bin/nvim
-# setup color schema
+
+# setup neovim color schema
 cd ~/.dotfiles
 git clone https://github.com/cocopon/iceberg.vim
 
@@ -54,4 +58,5 @@ ln -s -T ~/.vimrc ~/.config/nvim/init.vim
 ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
 mkdir -p ~/.config/nvim/colors
 ln -s -T ~/.dotfiles/iceberg.vim/colors/iceberg.vim ~/.config/nvim/colors/iceberg.vim
+
 source ~/.bash_profile
