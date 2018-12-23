e04b70e6d073a75b169a7dd39f433defdd6f055f
motchie8 <m.naoki1224@gmail.com>
2018-12-23 18:17:48 +0900

    added install script for ubuntu

---
+++ bin/install-apt.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# install libraries for pyenv and neovim
+sudo apt-get update -y && sudo apt-get install build-essential zlib1g-dev libssl-dev libreadline-dev libbz2-dev libsqlite3-dev wget cmake pkg-config unzip libtool libtool-bin m4 automake gettext zsh -y
+
+# install pyenv
+curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
+echo '
+export PATH="$HOME/.pyenv/bin:$PATH"
+export PYENV_PATH=$HOME/.pyenv
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+' >> ~/.bash_profile
+source ~/.bash_profile
+
+# create envs for neovim by pyenv-virtualenv
+pyenv install 2.7.15
+pyenv install 3.5.6
+pyenv virtualenv 2.7.15 neovim2
+pyenv virtualenv 3.5.6 neovim3
+
+# setup python libs for each env
+pyenv rehash
+source ~/.bash_profile
+pyenv activate neovim2
+pip install --upgrade pip
+pip install neovim
+pyenv deactivate
+pyenv activate neovim3
+pip install --upgrade pip
+pip install -r ~/.dotfiles/python/requirements.txt
+# create symbolic links
+ln -s `pyenv which flake8` ~/.pyenv/bin/flake8
+#ln -s `pyenv which flake8-import-order` ~/.pyenv/bin/flake8-import-order
+ln -s `pyenv which autopep8` ~/.pyenv/bin/autopep8
+#ln -s `pyenv which jedi` ~/.pyenv/bin/jedi
+ln -s `pyenv which isort` ~/.pyenv/bin/isort
+
+# install neovim
+cd ~/.dotfiles
+git clone https://github.com/neovim/neovim
+cd neovim
+make clean
+make CMAKE_BUILD_TYPE=Release
+sudo make install
+sudo ln -s -T /usr/local/bin/nvim /usr/bin/nvim
+
+# setup neovim color schema
+cd ~/.dotfiles
+git clone https://github.com/cocopon/iceberg.vim
+
+# symbolic links
+sudo ln -s -T /usr/local/bin/nvim /usr/bin/nvim
+ln -s -T ~/.dotfiles/vim ~/.vim
+ln -s -T ~/.dotfiles/.vimrc ~/.vimrc
+mkdir -p ~/.config/nvim
+ln -s -T ~/.vimrc ~/.config/nvim/init.vim
+ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
+mkdir -p ~/.config/nvim/colors
+ln -s -T ~/.dotfiles/iceberg.vim/colors/iceberg.vim ~/.config/nvim/colors/iceberg.vim
+
+source ~/.bash_profile
+ 
