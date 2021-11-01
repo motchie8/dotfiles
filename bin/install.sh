6f4064bc0e2bb8c6c875c5cdcae72e81aefdcfa8
motchie8 <m.naoki1224@gmail.com>
2021-11-02 01:55:18 +0900

    Update configs

--- bin/install.sh
+++ bin/install.sh
@@ -15,7 +15,7 @@ if [ "$OS" = "centos" ] || [ "$OS" = "amzn" ]; then
       readline-devel sqlite sqlite-devel openssl-devel xz \
       xz-devel libffi-devel make libtool autoconf automake \
       cmake gcc gcc-c++ make pkgconfig unzip xclip gettext \
-      patch ctags zsh zplug
+      patch ctags zsh
     # install neovim
     if ! type nvim >/dev/null 2>&1; then  
         echo "[INFO] install neovim for $OS"
@@ -50,15 +50,17 @@ elif [ "$OS" = "ubuntu" ]; then
     # set timezone
     TZ=Asia/Tokyo
     sudo ln -snf /usr/share/zoneinfo/$TZ /etc/localtime # && echo $TZ > /etc/timezone
-    export DEBIAN_FRONTEND=noninteractive
+    export DEBIAN_FEND=noninteractive
     sudo apt update -y && sudo apt install -y build-essential  
     sudo apt-key adv --refresh-keys --keyserver keyserver.ubuntu.com
+    sudo apt-get install -y language-pack-ja
     sudo apt-get install -y software-properties-common && sudo apt-get update -y
+    sudo update-locale LANG=ja_JP.UTF-8
     # install zsh, pyenv and vim plugin dependencies
     sudo apt install -y curl git file zlib1g-dev libssl-dev \
       libreadline-dev libbz2-dev libsqlite3-dev wget cmake \
       pkg-config unzip libtool libtool-bin m4 automake gettext \
-      zsh zplug x11-apps libffi-dev yarn
+      zsh x11-apps libffi-dev yarn
     # install neovim nightly
     # NOTE: nvim-treesitter needs Neovim nightly
     sudo add-apt-repository -y ppa:neovim-ppa/unstable  # ppa:neovim-ppa/stable
@@ -68,7 +70,7 @@ elif type sw_vers >/dev/null 2>&1; then
    brew update
    set +e
    # install pyenv, vim plugins and zsh
-   brew install node yarn wget tmux go zsh zplug fzf source-highlight gcc  # pyenv pyenv-virtualenv
+   brew install node yarn wget tmux go zsh fzf source-highlight gcc  # pyenv pyenv-virtualenv
    # install neovim nightly
    brew install --HEAD luajit
    brew install --HEAD neovim 
@@ -92,6 +94,8 @@ if ! type pyenv >/dev/null 2>&1; then
     export _OLD_VIRTUAL_PS1=""
     eval "$(pyenv init --path)"
     eval "$(pyenv virtualenv-init -)"
+elif type sw_vers >/dev/null 2>&1; then
+    brew upgrade pyenv
 else
   pyenv update
 fi
@@ -136,18 +140,26 @@ if [ "$DEFAULT_SHELL" != "zsh" ]; then
         echo "[INFO] Change default shell to zsh"
         echo $(which zsh) >> /etc/shells
     fi
-  # chsh -s /home/linuxbrew/.linuxbrew/bin/zsh
+    sudo chsh -s $(which zsh) $(whoami)
 fi
+
+# install zplug
+if [ ! -e $HOME/.dotfiles/.zplug ]; then
+    echo "[INFO] Install zplug"
+    export ZPLUG_HOME=$HOME/.dotfiles/.zplug
+    git clone https://github.com/zplug/zplug $ZPLUG_HOME
+fi
+
 
 # install zprezto and setup zsh dotfiles
 if [ ! -e $HOME/.dotfiles/.zprezto ]; then
-  echo "[INFO] Install zprezto"
-  git clone --recursive https://github.com/sorin-ionescu/prezto.git ~/.dotfiles/.zprezto
-  for rcfile_name in zlogin zlogout zpreztorc zprofile zshenv; do
-    if [ ! -e $HOME/$rcfile_name ]; then
-      ln -s "$HOME/.dotfiles/.zprezto/runcoms/.$rcfile_name" "$HOME/.$rcfile_name" 
-    fi
-  done
+    echo "[INFO] Install zprezto"
+    git clone --recursive https://github.com/sorin-ionescu/prezto.git ~/.dotfiles/.zprezto
+    for rcfile_name in zlogin zlogout zpreztorc zprofile zshenv; do
+        if [ ! -e $HOME/$rcfile_name ]; then
+            ln -s "$HOME/.dotfiles/.zprezto/runcoms/.$rcfile_name" "$HOME/.$rcfile_name" 
+        fi
+    done
 fi
 
 # download color schema for vim
@@ -186,7 +198,7 @@ fi
 if [ ! -L ~/.vimrc ]; then
   ln -s ~/.dotfiles/.vimrc ~/.vimrc
 fi
-if [ ! -e ~/.config/nvim ]; then
+if [ ! -L ~/.config/nvim/init.vim ]; then
   mkdir -p ~/.config/nvim 
   ln -s ~/.vimrc ~/.config/nvim/init.vim
 fi
