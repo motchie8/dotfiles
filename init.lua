47ed3bf7e83695c69b4514619cb3f2f2ed147ed2
motchie8 <m.naoki1224@gmail.com>
2023-10-06 00:45:11 +0900

    Modify init.lua to use Copilot.vim for commit messages

--- init.lua
+++ init.lua
@@ -1059,6 +1059,27 @@ require("packer").startup(function(use)
 	-- GitHub Copilot
 	use({
 		"github/copilot.vim",
+		config = function()
+			vim.g.copilot_filetypes = { yaml = true, markdown = true, gitcommit = true, gitrebase = true }
+
+			-- Complete commit messages with Copilot.vim
+			-- cf. https://zenn.dev/hasundue/articles/copilot-vim-commit-messages
+			vim.cmd([[
+                function s:append_diff() abort
+                  " Get the Git repository root directory
+                  let git_dir = FugitiveGitDir()
+                  let git_root = fnamemodify(git_dir, ':h')
+                  " Get the diff of the staged changes relative to the Git repository root
+                  let diff = system('git -C ' . git_root . ' diff --cached')
+                  " Add a comment character to each line of the diff
+                  let comment_diff = join(map(split(diff, '\n'), {idx, line -> '# ' . line}), "\n")
+                  " Append the diff to the commit message
+                  call append(line('$'), split(comment_diff, '\n'))
+                endfunction
+
+                autocmd BufReadPost COMMIT_EDITMSG call s:append_diff()
+            ]])
+		end,
 	})
 	-- Package Manager
 	if packer_bootstrap then
