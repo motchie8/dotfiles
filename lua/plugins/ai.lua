e8ba2122d74e6b1e9c269b268d77792f76bf561c
motchie8 <m.naoki1224@gmail.com>
2025-01-05 14:26:44 +0900

    Update default settings

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,78 +1,38 @@
 return {
-	-- Interactive Chat with GPT models
+	-- Interactive Chat with LLM models
 	{
 		"madox2/vim-ai",
 		event = "VeryLazy",
 		config = function()
-			-- load default configs from environment variables
-			local model = os.getenv("OPENAI_API_MODEL")
-			local endpoint_url = os.getenv("OPENAI_API_ENDPOINT_URL")
-			local default_role = os.getenv("VIM_AI_DEFAULT_ROLE")
-
-			local initial_chat_prompt = [[
-            >>> system
-            
-            You are a general assistant.
-            If you attach a code block add syntax type after ``` to enable syntax highlighting.
-            ]]
-
-			vim.g.vim_ai_chat = {
-				options = {
-					model = model,
-					endpoint_url = endpoint_url,
-					max_tokens = 1000,
-					temperature = 1,
-					request_timeout = 20,
-					selection_boundary = "",
-					initial_prompt = initial_chat_prompt,
-				},
-				ui = {
-					code_syntax_enabled = 1,
-					populate_options = 0,
-					open_chat_command = "preset_below",
-					scratch_buffer_keep_open = 0,
-					paste_mode = 1,
-				},
-			}
-			-- specify role config
+			-- Custom roles file location
 			vim.g.vim_ai_roles_config_file = "~/dotfiles/config/vim-ai-roles.ini"
 
-			-- Check if litellm is running
-			local function is_litellm_running()
-				local handle = io.popen("pgrep litellm")
-				-- if handle is nil, then litellm is not running
-				local result = handle:read("*a")
-				handle:close()
-				return result ~= ""
-			end
-
-			vim.api.nvim_create_user_command("AIProxyStart", function()
-				if not is_litellm_running() then
-					vim.fn.system("litellm --config ~/dotfiles/config/lite-llm-config.yaml &")
-					print("Started LiteLLM Proxy.")
-				else
-					print("LiteLLM Proxy is already running.")
-				end
-			end, {})
-
-			local function start_new_conversation_with_saving_chat()
+			-- Open new chat with conversation saving
+			vim.api.nvim_create_user_command("AISavingChat", function()
 				local chat_file_path = "~/vimwiki/aichat/"
 				local unique_id = vim.fn.system("uuidgen")
 				local timestamp = os.date("%Y-%m-%d_%H%M%S")
 				local aichat_filename = timestamp .. "_" .. string.sub(unique_id, 1, 8) .. ".aichat"
-				vim.cmd("AINewChat")
+				vim.cmd("AIChat")
 				vim.bo.buftype = ""
 				vim.cmd("saveas " .. chat_file_path .. aichat_filename)
-			end
-
-			vim.api.nvim_create_user_command("AISavingChat", function()
-				start_new_conversation_with_saving_chat()
 			end, {})
 
+			-- Continue chat. If default role is set, use it.
+			vim.api.nvim_create_user_command("AIC", function()
+				local default_role = os.getenv("VIM_AI_DEFAULT_ROLE")
+				if default_role == nil or default_role == "" then
+					vim.cmd("AIChat")
+				else
+					vim.cmd("AIChat /" .. default_role)
+				end
+			end, {})
+
+			-- Open new chat including the current buffer
 			vim.api.nvim_create_user_command("AIIncludingChat", function()
 				local bufnr = vim.api.nvim_get_current_buf()
 				local current_filename = vim.api.nvim_buf_get_name(bufnr)
-				start_new_conversation_with_saving_chat()
+				vim.cmd("AISavingChat")
 				vim.api.nvim_buf_set_lines(0, 0, 0, false, {
 					">>> include",
 					"",
@@ -81,8 +41,9 @@ return {
 				})
 			end, {})
 
+			-- Open new chat with the prompt for translation
 			vim.api.nvim_create_user_command("AITranslationChat", function()
-				start_new_conversation_with_saving_chat()
+				vim.cmd("AISavingChat")
 				vim.api.nvim_buf_set_lines(0, 0, 0, false, {
 					">>> system",
 					"",
@@ -91,33 +52,22 @@ return {
 				})
 			end, {})
 
-			vim.api.nvim_create_user_command("AIFileTranslation", function()
-				local bufnr = vim.api.nvim_get_current_buf()
-				local current_filename = vim.api.nvim_buf_get_name(bufnr)
-				start_new_conversation_with_saving_chat()
-				-- Delete all lines in the buffer
-				local current_buf = vim.api.nvim_get_current_buf()
-				local line_count = vim.api.nvim_buf_line_count(current_buf)
-				vim.api.nvim_buf_set_lines(current_buf, 0, line_count, false, {})
-				-- Add lines to the top of buffer
-				vim.api.nvim_buf_set_lines(0, 0, 0, false, {
-					">>> user",
-					"",
-					"Please write the content of the following file in natural English.",
-					"",
-					">>> include",
-					"",
-					current_filename,
-					"",
-				})
-			end, {})
-			vim.api.nvim_set_keymap("n", "Te", "<Cmd>AIEdit translate into english<CR>", { noremap = true })
-			vim.api.nvim_set_keymap("x", "Te", "<Cmd>AIEdit translate into english<CR>", { noremap = true })
-			vim.api.nvim_create_user_command("AIC", function()
-				if default_role == nil or default_role == "" then
-					vim.cmd("AIChat")
+			-- Check if litellm is running
+			local function is_litellm_running()
+				local handle = io.popen("ps aux | grep '[l]itellm'")
+				-- if handle is nil, then litellm is not running
+				local result = handle:read("*a")
+				handle:close()
+				return result ~= ""
+			end
+
+			-- Start litellm proxy if not running
+			vim.api.nvim_create_user_command("AIProxyStart", function()
+				if not is_litellm_running() then
+					vim.fn.system("litellm --config ~/dotfiles/config/lite-llm-config.yaml --port 4000 &")
+					print("Started LiteLLM Proxy.")
 				else
-					vim.cmd("AIChat /" .. default_role)
+					print("LiteLLM Proxy is already running.")
 				end
 			end, {})
 		end,
@@ -137,7 +87,7 @@ return {
 	-- Copilot chat
 	{
 		"CopilotC-Nvim/CopilotChat.nvim",
-		branch = "canary",
+		branch = "main",
 		event = "VeryLazy",
 		dependencies = {
 			{ "github/copilot.vim" },
