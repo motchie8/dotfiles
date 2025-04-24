abc9309564fb18ed949e70d27b1f81e738a324c9
motchie8 <m.naoki1224@gmail.com>
2025-04-24 23:04:06 +0900

    Update model configs

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -22,7 +22,7 @@ return {
 					temperature = 1.0,
 					request_timeout = 20,
 					stream = 1,
-					enable_auth = 0,
+					auth_type = "none",
 					token_file_path = "",
 					selection_boundary = "#####",
 					initial_prompt = default_complete_prompt,
@@ -149,12 +149,12 @@ return {
 		-- Example key mappings for common actions:
 		keys = {
 			{ "<leader>a", "<cmd>Aider toggle<cr>", desc = "Toggle Aider" },
-			{ "<leader>as", "<cmd>Aider send<cr>", desc = "Send to Aider", mode = { "n", "v" } },
-			{ "<leader>ac", "<cmd>Aider command<cr>", desc = "Aider Commands" },
-			{ "<leader>ab", "<cmd>Aider buffer<cr>", desc = "Send Buffer" },
-			{ "<leader>a+", "<cmd>Aider add<cr>", desc = "Add File" },
-			{ "<leader>a-", "<cmd>Aider drop<cr>", desc = "Drop File" },
-			{ "<leader>ar", "<cmd>Aider add readonly<cr>", desc = "Add Read-Only" },
+			-- { "<leader>as", "<cmd>Aider send<cr>", desc = "Send to Aider", mode = { "n", "v" } },
+			-- { "<leader>ac", "<cmd>Aider command<cr>", desc = "Aider Commands" },
+			-- { "<leader>ab", "<cmd>Aider buffer<cr>", desc = "Send Buffer" },
+			-- { "<leader>a+", "<cmd>Aider add<cr>", desc = "Add File" },
+			-- { "<leader>a-", "<cmd>Aider drop<cr>", desc = "Drop File" },
+			-- { "<leader>ar", "<cmd>Aider add readonly<cr>", desc = "Add Read-Only" },
 			-- Example nvim-tree.lua integration if needed
 			{ "<leader>a+", "<cmd>AiderTreeAddFile<cr>", desc = "Add File from Tree to Aider", ft = "NvimTree" },
 			{ "<leader>a-", "<cmd>AiderTreeDropFile<cr>", desc = "Drop File from Tree from Aider", ft = "NvimTree" },
