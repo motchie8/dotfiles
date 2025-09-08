96a1313dc3a86901a84c550a279ddcafc2d9efe7
motchie8 <m.naoki1224@gmail.com>
2025-09-09 00:45:47 +0900

    Update diffview keymaps

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,20 +1,38 @@
 return {
-	-- Git commands and Gdiffsplit
-	{
-		"tpope/vim-fugitive",
-		event = "VeryLazy",
-	},
-	-- Git decoration
-	{
-		"lewis6991/gitsigns.nvim",
-		config = function()
-			require("gitsigns").setup()
-		end,
-	},
-	-- Git diffview
-	{
-		"sindrets/diffview.nvim",
-		cmd = "DiffviewOpen",
-		dependencies = { "nvim-lua/plenary.nvim" },
-	},
+    -- Git commands and Gdiffsplit
+    {
+        "tpope/vim-fugitive",
+        event = "VeryLazy",
+    },
+    -- Git decoration
+    {
+        "lewis6991/gitsigns.nvim",
+        config = function()
+            require("gitsigns").setup()
+        end,
+    },
+    -- Git diffview
+    {
+        "sindrets/diffview.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        event = "VeryLazy",
+        config = function()
+            require("diffview").setup({
+                view = {
+                    default = {
+                        layout = "diff2_vertical",
+                        -- layout = "diff3_mixed",
+                    },
+                },
+            })
+            vim.api.nvim_set_keymap("n", "<Leader>GD", "<Cmd>DiffviewOpen<CR>",
+                { noremap = true, silent = true })
+            vim.api.nvim_set_keymap("n", "<Leader>GF", "<Cmd>DiffviewFileHistory<CR>",
+                { noremap = true, silent = true })
+            vim.api.nvim_set_keymap("n", "<Leader>Gf", "<Cmd>DiffviewFileHistory %<CR>",
+                { noremap = true, silent = true })
+            vim.api.nvim_set_keymap("n", "<Leader>GP", "<Cmd>DiffviewOpen origin/HEAD...HEAD --imply-local<CR>",
+                { noremap = true, silent = true })
+        end
+    },
 }
