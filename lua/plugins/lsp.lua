534ca324559e3a716cfe4ecc1c2804b49f80351c
motchie8 <m.naoki1224@gmail.com>
2025-09-15 15:38:13 +0900

    Update keymap

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -76,9 +76,9 @@ return {
 
                     vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
                     vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-                    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+                    vim.keymap.set("n", "<C-k>", vim.lsp.buf.hover, opts)
                     vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-                    vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+                    vim.keymap.set("n", "<C-s>", vim.lsp.buf.signature_help, opts)
                     vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
                     vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
                     -- Use actions-preview.nvim plugin for code actions
