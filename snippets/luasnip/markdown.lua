3dae904a8235f58391f01b0d27876a9c6d683a5b
motchie8 <m.naoki1224@gmail.com>
2025-09-22 22:49:09 +0900

    Add snippet

--- snippets/luasnip/markdown.lua
+++ snippets/luasnip/markdown.lua
@@ -3,23 +3,23 @@ local s = ls.snippet
 local i = ls.insert_node
 local fmt = require("luasnip.extras.fmt").fmt
 local function get_today_date()
-	local today = os.date("*t")
-	return string.format("%04d-%02d-%02d", today.year, today.month, today.day)
+    local today = os.date("*t")
+    return string.format("%04d-%02d-%02d", today.year, today.month, today.day)
 end
 local function get_weekday()
-	local today = os.date("*t")
-	local weekdays = { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" }
-	return weekdays[today.wday]
+    local today = os.date("*t")
+    local weekdays = { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" }
+    return weekdays[today.wday]
 end
 local function get_hhmm()
-	local today = os.date("*t")
-	return string.format("%02d:%02d", today.hour, today.min)
+    local today = os.date("*t")
+    return string.format("%02d:%02d", today.hour, today.min)
 end
 return {
-	s(
-		"schedule",
-		fmt(
-			[[
+    s(
+        "schedule",
+        fmt(
+            [[
 # Diary for {date}({weekday})
 
 ## Current active tasks | status.not:Completed and +ACTIVE
@@ -37,25 +37,50 @@ return {
 
 ## Memo
             ]],
-			{ date = i(1, get_today_date()), weekday = i(2, get_weekday()) }
-		)
-	),
-	s(
-		"mtg",
-		fmt(
-			[[
+            { date = i(1, get_today_date()), weekday = i(2, get_weekday()) }
+        )
+    ),
+    s(
+        "mtg",
+        fmt(
+            [[
                 {}: {}-{}
             ]],
-			{ i(1, "Meeting"), i(2, "09:00"), i(3, "10:00") }
-		)
-	),
-	s(
-		"task",
-		fmt(
-			[[
+            { i(1, "Meeting"), i(2, "09:00"), i(3, "10:00") }
+        )
+    ),
+    s(
+        "task",
+        fmt(
+            [[
                 [ ] {}: {}
             ]],
-			{ i(1, "task"), i(2, "min") }
-		)
-	),
+            { i(1, "task"), i(2, "min") }
+        )
+    ),
+    s(
+        "details",
+        fmt(
+            [[
+<!--
+{}
+-->
+		  ]],
+            { i(1) }
+        )
+    ),
+    s(
+        "details",
+        fmt(
+            [[
+<details>
+  <summary>{}</summary>
+
+{}
+
+</details>
+      ]],
+            { i(1), i(0) }
+        )
+    ),
 }
